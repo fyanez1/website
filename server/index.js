d2449347e7493c96a000ca7bebe49a3a86aeb43a
Fabian Yanez-Laguna <fyanez@mit.edu>
2023-02-05 22:14:58 -0500

    new db

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const api = require("./api");
 // const path = require('path')
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use("/api", api)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", api);
 
 require("dotenv").config();
 
 const mongoConnectionURL = process.env.MONGODB_URI;
-const databaseName = "ToDoList";
+const databaseName = "todo";
 
-mongoose.set('strictQuery', true);
-mongoose.connect(mongoConnectionURL, {
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(mongoConnectionURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: databaseName,
   })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`)
-);
+  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
 
 // if (process.env.NODE_ENV === "production") {
 //     app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -33,5 +33,5 @@ mongoose.connect(mongoConnectionURL, {
 
 // Initialize server
 app.listen(5000, () => {
-    console.log("Server running on port 5000.");
+  console.log("Server running on port 5000.");
 });
